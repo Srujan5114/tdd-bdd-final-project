eae468610ed63eaea4387ee7822ffd5e52275539
G Srujan <119472750+Srujan5114@users.noreply.github.com>
2026-04-18 23:36:13 +0530

    Add BDD load step to create products via REST API
    Implement product creation via REST API using data from context.table.

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ######################################################################
-
 """
 Product Steps
 
@@ -29,6 +28,7 @@ from behave import given
 HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
+
 
 @given('the following products')
 def step_impl(context):
@@ -47,6 +47,12 @@ def step_impl(context):
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert(context.resp.status_code == HTTP_201_CREATED)
